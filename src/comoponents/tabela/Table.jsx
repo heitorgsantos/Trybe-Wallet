b054638740567637030f1d51ab2de223ca835bbe
Heitor Santos <heitorg33@gmail.com>
2021-09-30 23:33:39 -0300

    Correçao do codigo

--- src/comoponents/tabela/Table.jsx
+++ src/comoponents/tabela/Table.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import './table.css';
+// import './table.css';
 
 class Table extends Component {
   render() {
